61fe86c976dc0bd9dd5a16ed658643a7c476ebf1
messuped <mrmessuped@gmail.com>
2017-03-25 15:51:13 +0000

    15:51

--- app/src/main/java/com/example/nunomorais/a123/Log.java
+++ app/src/main/java/com/example/nunomorais/a123/Log.java
@@ -1,10 +1,12 @@
-package com.example.messuped.equipa_20;
+package com.example.nunomorais.a123;
 
 /**
  * Created by messuped on 3/25/17.
  */
 
 public class Log {
+
+    //pensar na merda das ED
 
     public Log() {
 
