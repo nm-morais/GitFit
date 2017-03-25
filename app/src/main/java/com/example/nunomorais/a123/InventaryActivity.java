f9a6d16503983986222a20f98cc76b50d18f29b7
Leonardo <lc.correia@campus.fct.unl.pt>
2017-03-25 20:08:35 +0000

    First Commit

--- app/src/main/java/com/example/nunomorais/a123/InventaryActivity.java
+++ app/src/main/java/com/example/nunomorais/a123/InventaryActivity.java
@@ -3,14 +3,14 @@ package com.example.nunomorais.a123;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class LogActivity extends AppCompatActivity {
+public class InventaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log2);
+        setContentView(R.layout.activity_inventary);
+
         Intent intent = getIntent();
     }
 }
