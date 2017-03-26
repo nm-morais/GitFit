f7fa1a2ee3cc57f2e98548787dc8650891e00bfc
messuped <mrmessuped@gmail.com>
2017-03-26 07:18:27 +0100

    Subtil - Já não é tanta merda v4

--- app/src/test/java/com/example/nunomorais/GitFit/ExampleUnitTest.java
+++ app/src/test/java/com/example/nunomorais/GitFit/ExampleUnitTest.java
@@ -1,8 +1,13 @@
 package com.example.nunomorais.GitFit;
 
+import com.example.nunomorais.GitFit.Food.Food;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Iterator;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,23 +15,36 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-
+    Gitfit g = new Gitfit();
 
 
 
     public void testRecipes(){
+        g.addFood("liquid","agua",1,1,1,1);
+        g.addFood("liquid","aga",0,0,0,0);
+        g.addFood("liquid","aua",2,2,2,2);
+        g.addFood("liquid","a",4,4,4,4);
+        g.setFoodStock("agua", 3);
+        g.setFoodStock("a",2);
 
+        Iterator<Food> itAll = g.getAllFoods();
+        Iterator<Food> itAv = g.getAvailableRecipes()
+        Iterator<Food> itCal = g.getRecipiesByCal();
 
+        while(itAll.hasNext()){
+            System.out.println(itAll.next().getName());
+        }
+        System.out.println();
 
+        while(itAv.hasNext()){
+            System.out.println(itAll.next().getName());
+        }
+        System.out.println();
 
+        while(itCal.hasNext()){
+            System.out.println(itAll.next().getName());
+        }
+        System.out.println();
     }
 
-
-
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-        Gitfit top = new Gitfit();
-
-    }
 }
