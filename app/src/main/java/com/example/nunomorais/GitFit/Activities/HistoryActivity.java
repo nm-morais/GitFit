ea0440c2c19cc4eab1749f5e96c5c92d39c5009b
nunomorais <nm.morais@campus.fct.unl.pt>
2017-03-26 05:19:18 +0100

    First Commit

--- app/src/main/java/com/example/nunomorais/GitFit/Activities/HistoryActivity.java
+++ app/src/main/java/com/example/nunomorais/GitFit/Activities/HistoryActivity.java
@@ -11,7 +11,7 @@ public class HistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log);
+        setContentView(R.layout.activity_history);
 
         Intent intent = getIntent();
     }
