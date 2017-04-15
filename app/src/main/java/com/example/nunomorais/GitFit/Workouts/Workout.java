29161b0557b747915b57deed4ab27c518c016fa9
nunomorais <nm.morais@campus.fct.unl.pt>
2017-04-15 10:46:43 +0100

    redoing all the exercises and workouts

--- app/src/main/java/com/example/nunomorais/GitFit/Workouts/Workout.java
+++ app/src/main/java/com/example/nunomorais/GitFit/Workouts/Workout.java
@@ -1,12 +1,39 @@
 package com.example.nunomorais.GitFit.Workouts;
 
 
-//TODO
+import java.util.Iterator;
+
+//TODO if a user inserts a new exercise with the number 5 and there is an exercise with the number 5
+//TODO the old exercise must go to 6 and so on.
+//todo implement a comparator, and fix collisions
+//todo implement a way for the user to reorder the exercises, as order is important in a workout
+
 interface Workout {
 
-    void addExerciseToWorkout(Exercise e);
+
+    /**
+     * adds a new exercise to the workout
+     *
+     * @param e               Object exercise
+     * @param exercise_number relevant to the order.
+     */
+    void addExerciseToWorkout(Exercise e, int exercise_number);
+
+    /**
+     * iterates through all th exercises in the workout
+     *
+     * @return Object Iterator
+     */
+    Iterator<Exercise> getAllExercises();
 
 
-
+    /**
+     * returns the exercise with the key equal to the inserteg number
+     * the exercises are sorted by key so that the user can define the order
+     *
+     * @param exercise_number the number of the exercise (for order)
+     * @return Object Exercise
+     */
+    Exercise getExercise (int exercise_number);
 
 }
--- app/src/main/java/com/example/nunomorais/GitFit/Workouts/WorkoutClass.java
+++ app/src/main/java/com/example/nunomorais/GitFit/Workouts/WorkoutClass.java
@@ -1,6 +1,32 @@
 package com.example.nunomorais.GitFit.Workouts;
 
-//TODO
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+//TODO fix collisions ???
 public class WorkoutClass implements Workout {
 
+
+    Map<Integer, Exercise> exercises;
+
+
+    public WorkoutClass() {
+        this.exercises = new TreeMap<>();
+    }
+
+    @Override
+    public void addExerciseToWorkout(Exercise e, int exercise_number) {
+        this.exercises.put(exercise_number, e);
+    }
+
+    @Override
+    public Iterator<Exercise> getAllExercises() {
+        return exercises.values().iterator();
+    }
+
+    @Override
+    public Exercise getExercise(int exercise_number) {
+        return exercises.get(exercise_number);
+    }
 }
